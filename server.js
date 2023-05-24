9dd1dd1a08b66d2fca6c7d5a9d0d53ce1faebf51
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-05-24 15:24:58 -0700

    TA_assist

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 const Book = require('./models/books');
-
+mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Mongoose is connected');
 });
 
-mongoose.connect(process.env.DB_URL);
+
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
-
+app.get('/addseeds', (req,res) => seed(req,res))
 app.get('/books', getBooks);
 
 async function getBooks(req, res, next) {
   try {
-    let results = await Book.find();
+    let results = await Book.find({});
     res.status(200).send(results);
   } catch(err) {
     next(err)
