55add807959de481eac832281926457ec7c30345
Daniel Quinn <dandidit.quinn@gmail.com>
2023-05-29 11:04:06 -0700

    4andrew

---
+++ auth.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//jwt -json web token - pronounced jot
+//install with npm i jsonwebtoken
+const jwt = require('jsonwebtoken');
+
+//jwks - web key set jawicks
+
+//install with npm i jwks-rsa
+
+const jwksClient = require('jwks-rsa')
+
+const client = jwksClient({
+  jwksUri: process.env.JWKS_URI
+});
+
+// getkewy function to make things work
+// this comes from the jsonwebtoken docs
+// https://www.npmjs.com/package/jsonwebtoken (search for auth0)
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+// we need to verfy that the user on our route is who they say
+function verifyUser(req, errorFirstOrUserCallbackFunction) {
+  try {
+    // extract the token from the user's request
+    const token = req.headers.authorization.split(' ')[1];
+    console.log(token);
+    // from jsonwebtoken docs
+    jwt.verify(token, getKey, {}, errorFirstOrUserCallbackFunction)
+  } catch(error) {
+    errorFirstOrUserCallbackFunction('not authorized');
+  }
+}
+
+module.exports = verifyUser;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const Book = require('./models/books');
+//require verify user to update tokens
+const verifyUser = require('./auth')
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -17,6 +19,8 @@ mongoose.connect(process.env.DB_URL);
 const app = express();
 app.use(cors());
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (request, response) => {
@@ -26,6 +30,7 @@ app.get('/', (request, response) => {
 app.get('/books', getBooks);
 app.post('/books', postBooks);
 app.delete('/books/:id', deleteBooks);
+app.put('/books/:id', putBooks);
 
 async function getBooks(req, res, next) {
   try {
@@ -38,8 +43,8 @@ async function getBooks(req, res, next) {
 
 async function postBooks(req, res, next) {
   try {
-    let createdBook = await Books.create(req.body);
-    res.status(200).send(created Book);
+    let createdBook = await Book.create(req.body);
+    res.status(200).send(createdBook);
   } catch(err) {
     next(err)
   }
@@ -47,13 +52,29 @@ async function postBooks(req, res, next) {
 
 async function deleteBooks(req, res, next) {
   try {
-    let createdBook = await Books.create(req.body);
-    res.status(200).send(Deleted Book);
+    let id = req.params.id;
+    await Book.findByIdAndDelete(id);
+    res.status(200).send('Deleted Book');
   } catch(err) {
     next(err)
   }
 }
 
+async function putBooks(req, res, next) {
+  try {
+    let id = req.params.id;
+    let bookFormReq = req.body;
+    let options = {
+      new: true,
+      overwrite: true,
+
+    }
+    let updatedBook = await Book.findByIdAndPut(id, bookFormReq, options);
+    res.status(200).send(updatedBook);
+  } catch(err) {
+    next(err)
+  }
+}
 app.get('*', (request, response) => {
   response.status(404).send('Not availabe');
 });
